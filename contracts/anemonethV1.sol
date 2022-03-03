3e277f3cc57f4b3292b70663503e2d33adcf4c54
Joey Marino <joeymarino94@gmail.com>
2022-03-03 12:20:13 -0600

    now compiling

--- contracts/anemonethV1.sol
+++ contracts/anemonethV1.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20CappedUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-// import "@nomiclabs/hardhat-ethers";
 
 
 contract AnemonethV1 is ERC20CappedUpgradeable, OwnableUpgradeable {
@@ -43,17 +42,18 @@ contract AnemonethV1 is ERC20CappedUpgradeable, OwnableUpgradeable {
         users.push(User({ addr: msg.sender, username: _username, joinDate: block.timestamp}));
     }
 
-    function weeklyEarnings(User[] memory _userArr, uint amount_) public view onlyOwner {
+    function weeklyEarnings() public onlyOwner {
         // Calculate how much NEM to give to each EAO and how much total NEM to mint
         // This will be hard. Each post/comment/interaction cannot be an eth tx due to prohibitive
         // tx costs. We will have to aggregate IPFS data for each user and somehow get that data
         // into the contract... total mint hardcoded for now at 1000 and a fake user will be given 
         // it
-        thisWeek = WeeklyInfo(weeklyInfoArr.length, 1000);
+        WeeklyInfo memory thisWeek = WeeklyInfo(weeklyInfoArr.length, 1000);
         for (uint i=0; i<users.length; i++) {
-            historicalEarnings[user[i].address][thisWeek.weekNumber] = 1000;
+            address userHist = users[i].addr;
+            historicalEarnings[userHist][thisWeek.weekNumber] = 1000;
         }
-        require( (thisWeek.weekNumber >= ( (weeklyInfoArr.length-1) + 1 week )) || weeklyInfoArr.length == 0);
+        require( (thisWeek.weekNumber >= ( (weeklyInfoArr.length-1) + 604800 )) || weeklyInfoArr.length == 0);
         weeklyInfoArr.push(thisWeek);
         // we need to emit an event here and check for it in the mint function. Otherwise something might go wrong, it doesnt update weeklyInfoArr and mint() would mint last weeks amount again
     }
@@ -69,7 +69,8 @@ contract AnemonethV1 is ERC20CappedUpgradeable, OwnableUpgradeable {
         // reference ERC20Upgradable line 231
         for (uint i=0; i<users.length; i++) {
             address to = users[i].addr;
-            uint amount = historicalEarnings[user[i].address][weeklyInfoArr[weeklyInfoArr.length]];
+            uint weekNumber = weeklyInfoArr[weeklyInfoArr.length].weekNumber;
+            uint amount = historicalEarnings[users[i].addr][weekNumber];
             _transfer(address(this), to, amount);
         }
     }
