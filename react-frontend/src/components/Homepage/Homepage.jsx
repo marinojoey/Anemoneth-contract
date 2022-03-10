f3672eb36d7fd9892a53811e53112af810bd6930
Joey Marino <joeymarino94@gmail.com>
2022-03-10 17:03:37 -0600

    web3.storage is working

--- react-frontend/src/components/Homepage/Homepage.jsx
+++ react-frontend/src/components/Homepage/Homepage.jsx
@@ -1,57 +1,44 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import './homepage.scss';
-import { ethers } from 'ethers';
-import anemonethJSON from '../../utils/anemoneth.json'
+import { Web3Storage, File } from 'web3.storage'
+
+let web3s = new Web3Storage({
+  token: process.env.REACT_APP_WEB3STORAGE_TOKEN
+});
+
+let fileName = 'plain-utf8.txt'
+async function storeFiles() {
+  const string = document.querySelector('.str').value;
+  const file = new File([string], fileName);
+  const cid = await web3s.put([file]);
+  console.log('stored files with cid:', cid)
+  return cid
+}
+
+async function retrieveFiles(cid) {
+  const res = await web3s.get(cid)
+  console.log(`Got a response! [${res.status}] ${res.statusText}`)
+  if (!res.ok) {    
+    throw new Error(`failed to get ${cid}`)  
+  }
+  const files = await res.files()
+  const file = files[0]
+  let fileText = await file.text();
+  console.log(fileText)
+}
 
 function Homepage() {
-
-  const anemonethProxyAddress = "0x25a0C89a55dfF4b779bde0DDa7897F6Ef06e6565";
-  // const anemonethImpAddress = "0x4a391779abcc217c3beab96a639934116069b830";
-
-  // const [address, setAddress] = React.useState("");
-  // const [balance, setBalance] = React.useState(0);
-  // // const [nemBalance, setNemBalance] = React.useState(0);
-
-  // const { ethereum } = window;
-  // let provider;
-
-  // if(ethereum) {
-  //   ethereum.request({ method: 'eth_requestAccounts'});
-  //   provider = new ethers.providers.Web3Provider(ethereum);
-  //   getUserDetails();
-  // } else {
-  //   console.log("Metamask not found. Pleast install MetaMask!")
-  // }
-
-  // async function getUserDetails() {
-  //   const signer = await provider.getSigner();
-  //   const addr = await signer.getAddress();
-  //   const userBalance = await provider.getBalance(addr);
-  //   setAddress(addr);
-  //   setBalance(ethers.utils.formatEther(userBalance));
-  // }
-
-  // async function callRegister() {
-  //   let regAmt = parseInt(document.querySelector('.regAmt').value);
-  //   const username = document.querySelector('.usrnm').value;
-  //   console.log(`register amount is: ${regAmt}. Username is: ${username}`);
-  //   console.log(typeof(regAmt))
-  //   console.log(typeof(username))
-  //   if (regAmt >= .000000001 ) {
-  //     const signer = await provider.getSigner();
-  //     const contractInstance = new ethers.Contract(anemonethProxyAddress, anemonethJSON.abi, signer);
-
-  //     // const overrides = {
-  //     //   value: regAmt,
-  //     //   gasLimit: 230000 
-  //     // };
-
-  //     await contractInstance.register(username, { value: regAmt, gasLimit: 125000 });
-  //   } else console.log("Increase msg.value")
-  // }
+  // ( () => { makeStorageClient() }) ();;
   return (
       <>    
         <h1>HOMEPAGE</h1>
+        <label htmlFor='str' className='strlbl'></label>
+        <input type="text" className='str' placeholder='web3.storage'></input>
+        <button className='stringbtn' onClick={ storeFiles }>Post</button>
+        <br/>
+        <input type="text" className='cid' placeholder='Content Identifier'></input>
+        <button className='retrieve' onClick={ () => retrieveFiles(document.querySelector('.cid').value) }>Retrieve</button>
+        <div className='respone'></div>
       </>
   )
 }
